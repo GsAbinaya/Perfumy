2043fca813efb77cfe2a98c1a5de50ceca10b49f
AbinayaSethuramachandran <abinayapraveenkumar26@gmail.com>
2025-06-21 11:17:16 +0200

    Trigger Vercel redeploy

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
       <div className="container mx-auto gap-4 p-4 bg-black text-white">
       <h1 className="text-center text-white font-extrabold p-4 text-2xl">About us</h1>
       <p className="p-4 text-wrap">Lorem ipsum dolor sit amet consectetur adipisicing elit. A ipsum necessitatibus explicabo. Quaerat corrupti nihil molestiae expedita cum officiis ipsa, 
-        sed est unde placeat nisi dolores magnam perspiciatis illo libero ab. 
+        sed est placeat nisi dolores magnam perspiciatis illo libero ab. 
         Voluptas, commodi eligendi. Molestiae, perspiciatis quia consequuntur 
         earum assumenda, ipsam culpa tempora facere facilis cupiditate asperiores. 
         Non consequuntur velit iusto doloremque reiciendis distinctio culpa, deleniti fugit 
